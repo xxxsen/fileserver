4739b5920a66a16590e64a7615d6b8c6b8818e0b
xxxsen <naivesen@gmail.com>
2022-10-11 00:44:24 +0800

    feat: dont push docker image

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-. VERSION
-
-docker buildx build -t xxxsen/file_server:${VERSION} -t xxxsen/file_server:latest --platform=linux/amd64,linux/arm64 . --push
+docker build -t xxxsen/file_server:test .
